a30c21113aa57394432b972cabd5abdabfb7d56a
Taejun Park <ekswn120@gmail.com>
2023-12-13 19:32:48 +0900

    Initial commit

---
+++ src/main/java/org/jnjeaaaat/onbition/OnbitionApplication.java
@@ -0,0 +1,13 @@
+package org.jnjeaaaat.onbition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnbitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnbitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jnjeaaaat/onbition/OnbitionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jnjeaaaat.onbition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnbitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
